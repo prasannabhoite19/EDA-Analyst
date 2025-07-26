5783eb8ae4cd579a12bded403f916d5a3d81d208
prasannabhoite19 <bhoiteprasanna19@gmail.com>
2025-07-26 17:58:58 +0530

    Delete agent/agents.wrappers.py

--- agent/agents.wrappers.py
+++
@@ -1,9 +0,0 @@
-from langchain_core.tools import tool
-from agent.tools import DataLoaderTool
-from agent.visualizer import plot_histogram, plot_corr_matrix, bar_chart
-
-loader = DataLoaderTool()
-loader.load_csv("data/sample.csv")
-loader.run_eda()
-
-df = loader.get_dataframe()
